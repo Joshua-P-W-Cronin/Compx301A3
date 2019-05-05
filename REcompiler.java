34c109b38d91e3e8ef2bc8556a754918cb9b18a3
Luke Weston <luke@weston.net.nz>
2019-05-05 16:35:36 +1200

    Fixed bugs

--- REcompiler.java
+++ REcompiler.java
@@ -1,13 +1,31 @@
 public class REcompiler {
 
-    public static void main(String[] args) {
+    //Regular expression phrase
+    static char[] p;
+    //counter of the index we are up to on the regular expression
+    static int j;
+    //Lists of states
+    static int[] next1, next2;
+    //list of chars
+    static char[] ch;
+    //Current State
+    static int state;
+    //Branching state
+    static final char BR = '\0';
 
-        //Do stuff
+    public static void main(String[] args) {
+        p = args[0].toCharArray();
+        j = 0;
+        next1 = new int[100];
+        next2 = new int[100];
+        ch = new char[100];
+        state = 0;
+
     }
 
 
-    public int factor() {
-        int r;
+    public static int factor() {
+        int r = 0;
         if (isVocab(p[j])) {
             setState(state, p[j], state + 1, state + 1);
             j++;
@@ -20,26 +38,29 @@ public class REcompiler {
             r = expression();
             if (p[j] == ')') {
                 j++;
-            } else {
-                error()
             }
-
+            else {
+                error();
+            }
         }
+
+        return r;
     }
 
 
-    public int expression() {
+    public static int expression() {
 
         int r;
         r = term();
-        if (Vocab(p[j]) || p[j] == '(') {
+        if (isVocab(p[j]) || p[j] == '(') {
             expression();
         }
         return r;
     }
 
 
-    public int term() {
+    public static int term() {
+        int f, r, t1, t2;
         f = state - 1;
         r = t1 = factor();
         if (p[j] == '*') {
@@ -70,11 +91,29 @@ public class REcompiler {
     }
 
 
-    public void setState(int s, char c, int s1, int s2) {
+    public static void setState(int s, char c, int s1, int s2) {
         ch[s] = c;
         next1[s] = s1;
         next2[s] = s2;
     }
+
+
+    public static void error(){
+        System.out.println("Regular expression invalid");
+        System.exit(0);
+
+    }
+
+    public static boolean isVocab(char c){
+
+        char[] illegalChars = new char[] {'(',')','.','|','*','^','[',']','?','\\'};
+
+        return !String.valueOf(illegalChars).contains(Character.toString(c));
+
+    }
+
+
+
 }
 
 
