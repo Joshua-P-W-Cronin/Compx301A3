a32c0e426b726fea09ece34de57a40e3ea1ce284
Joshua Cronin <joshuapwcronin@gmail.com>
2019-05-08 11:33:49 +1200

    Fixed some broken methods,  can now run the compiler, although certain aspects are still broken eg the | operator

--- REcompiler.java
+++ REcompiler.java
@@ -20,9 +20,28 @@ public class REcompiler {
         next2 = new int[100];
         ch = new char[100];
         state = 0;
-
+        parse();
+        printMachine();
     }
 
+    public static void parse()
+    {
+      int initial;
+
+      initial = expression();
+      //if (p[j])
+      // error(); // In C, zero is false, not zero is true
+      setState(state, ' ', 0, 0);
+      System.out.println("starting state = " + initial);
+    }
+
+    public static void printMachine(){
+      System.out.println("s  ch 1 2");
+      System.out.println("--+--+-+-+");
+      for(int i = 0; i<100; i++){
+        System.out.println(String.format("%1s | %2s %3s %4s", i, ch[i], next1[i], next2[i]));
+      }
+    }
 
     public static int factor() {
         int r = 0;
@@ -43,7 +62,6 @@ public class REcompiler {
                 error();
             }
         }
-
         return r;
     }
 
@@ -52,8 +70,10 @@ public class REcompiler {
 
         int r;
         r = term();
-        if (isVocab(p[j]) || p[j] == '(') {
+        if (j < p.length) {
+          if (isVocab(p[j]) || p[j] == '(') {
             expression();
+          }
         }
         return r;
     }
@@ -63,29 +83,31 @@ public class REcompiler {
         int f, r, t1, t2;
         f = state - 1;
         r = t1 = factor();
-        if (p[j] == '*') {
-            j++;
-            setState(state, BR, t1, state + 1);
-            r = state;
-            state++;
-        }
-        if (p[j] == '|') {
-            if (next1[f] == next2[f]) {
-                next2[f] = state;
-            }
+        if(j<p.length){
+          if (p[j] == '*') {
+              j++;
+              setState(state, BR, t1, state + 1);
+              r = state;
+              state++;
+          }
+          if (p[j] == '|') {
+              if (next1[f] == next2[f]) {
+                  next2[f] = state;
+              }
 
-            next1[f] = state;
-            f = state - 1;
-            j++;
-            r = state;
-            state++;
-            t2 = term();
-            setState(r, BR, t1, t2);
-            if (next1[f] == next2[f]) {
-                next2[f] = state;
+              next1[f] = state;
+              f = state - 1;
+              j++;
+              r = state;
+              state++;
+              t2 = term();
+              setState(r, BR, t1, t2);
+              if (next1[f] == next2[f]) {
+                  next2[f] = state;
 
-            }
-            next1[f] = state;
+              }
+              next1[f] = state;
+          }
         }
         return r;
     }
@@ -115,5 +137,3 @@ public class REcompiler {
 
 
 }
-
-
